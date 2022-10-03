b392b6e84dfbc5f732757695794adb741da3da44
macm <mac.mitsch@comcast.net>
2022-10-03 15:40:15 -0500

    finished mutations for login save book and remove book

--- sledgehammern/Develop/server/schemas/resolvers.js
+++ sledgehammern/Develop/server/schemas/resolvers.js
@@ -11,6 +11,45 @@ const resolvers = {
                 return userData;
             }
             throw new AuthentificationError('Please log in');
+        },
+    },
+    Mutation: {
+        addUser:async(parent,args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login:async (parent,{email,password}) => {
+            const user = await User.findOne({ email });
+            if(!user) {
+                throw new AuthentificationError('Incorrect credentials')
+            }
+            const token = signToken(user);
+            return {token, user };
+        },
+        saveBook: async (parent,{book}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id},
+                    { $addToSet: { savedBooks:book }},
+                    {new:true}
+                )
+                return updatedUser;
+            }
+            throw new AuthentificationError('Please log in')
+        },
+        removeBook: async (parent,{bookId},context) => {
+            if (context.user){
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id},
+                    {$pull: { savedBooks: { bookId:bookId}}},
+                    {new:true}
+                )
+                return updatedUser;
+            }
         }
     }
-}
+};
+
+module.exports = resolvers;
