3c0e1052d2e0f5bb0d279b45378846ac63f161d2
macm <mac.mitsch@comcast.net>
2022-10-03 16:45:23 -0500

    added Auth Mutations and saved Book to def

--- sledgehammern/Develop/server/schemas/typeDefs.js
+++ sledgehammern/Develop/server/schemas/typeDefs.js
@@ -1,6 +1,30 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        _id: ID
+        Username: String
+        email: String
+        bookCount:Int
+        savedBooks:[Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book:SavedBook): User
+        removeBook(bookId: String!): User
+    }
+
     type Book{
         _id:ID
         bookId:String
@@ -11,12 +35,16 @@ const typeDefs = gql`
         forSale: String
         link: String
     }
-    type User {
-        _id: ID
-        Username: String
-        email: String
-        bookCount:Int
-        savedBooks:[Book]
-    }
+    input SavedBook {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        forSale: String
+        link: String
 
-`
+    }
+`;
+
+module.exports = typeDefs;
